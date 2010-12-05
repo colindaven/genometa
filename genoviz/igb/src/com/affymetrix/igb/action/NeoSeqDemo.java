ec5a24d4f38d0287fbb85015ebee1db93631e53e
wienhoef <wienhoef@a82c77b2-4dfe-7ae9-12ac-60ed84af8d34>
2010-12-05 11:17:22 +0000

    [merging genoviz commit 7117-7122] vbishnoi changes to NeoSeqDemo.java
    original comment:
    --modified font and screen size of sequence viewer
    --corrected the sequence anamoly in exons and introns
    --corrected the first white space problem in sequence viewer and removed all the debug statements
    --color change white and yellow
    --interchanged yellow and white colors
    --added reverse complement

--- genoviz/igb/src/com/affymetrix/igb/action/NeoSeqDemo.java
+++ genoviz/igb/src/com/affymetrix/igb/action/NeoSeqDemo.java
@@ -78,6 +78,7 @@ public class NeoSeqDemo extends Applet
 	private boolean framesShowing = true;
 	private boolean going = false;
 	private Color nicePaleBlue = new Color(180, 250, 250);
+	NeoPanel widg_pan = new NeoPanel();
 
 	public void init(final SeqSpan[] spans1) {
 
@@ -141,48 +142,31 @@ public class NeoSeqDemo extends Applet
 
 	private void customFormatting(String[] seqArray, String[] intronArray, SeqSpan[] spans, String seq) {
 		int i = 1;
-//		System.out.println(" " + seq + "  " + seq.length() + " " + spans.length);
-//		String[] seqArray = new String[spans.length];
-//		String[] intronArray = new String[spans.length - 1];
 		seqArray[0] = seq.substring(0, spans[0].getLength());
 		if (spans.length > 1) {
 			if (spans[0].getStart() < spans[0].getEnd()) {
-				intronArray[0] = seq.substring(spans[0].getLength() + 1, spans[i].getStart() - spans[0].getStart() - 1);
+				intronArray[0] = seq.substring(spans[0].getLength(), spans[i].getStart() - spans[0].getStart());
 			} else {
-				intronArray[0] = seq.substring(spans[0].getLength() + 1, Math.abs(spans[i].getStart() - spans[0].getStart() + 1));
+				intronArray[0] = seq.substring(spans[0].getLength(), Math.abs(spans[i].getStart() - spans[0].getStart()));
 			}
 		}
-		System.out.println(0 + "  length " + spans[0].getLength() + " " + 0 + " " + spans[0].getLength());
 		while (i < spans.length) {
 			if (spans[i].getStart() < spans[i].getEnd()) {
-//			System.out.println(i + "  length " +spans[i].getLength() +" "+ (spans[i].getStart()-spans[0].getStart()) +" "+ (spans[i].getStart()-spans[0].getStart()+spans[i].getLength()));
-//				System.out.println("Extron " + i + "  length " + spans[i].getLength() + " " + (Math.abs(spans[i].getStart() - spans[0].getStart())) + " " + (Math.abs(spans[i].getStart() - spans[0].getStart() + spans[i].getLength())));
-
-				seqArray[i] = seq.substring(Math.abs(spans[i].getStart() - spans[0].getStart()), Math.abs(spans[i].getStart() - spans[0].getStart() + spans[i].getLength()));
+				seqArray[i] = seq.substring(Math.abs(spans[i].getStart() - spans[0].getStart()), Math.abs(spans[i].getEnd() - spans[0].getStart()));
 				if (i < spans.length - 1) {
-					intronArray[i] = seq.substring(spans[i].getEnd() - spans[0].getStart() + 1, spans[i + 1].getStart() - spans[0].getStart() - 1);
-//					System.out.println("Intron" + i + "  length " + spans[i].getLength() + " " + (spans[i].getEnd() - spans[0].getStart() + 1) + " " + (spans[i + 1].getStart() - spans[0].getStart() - 1));
+					intronArray[i] = seq.substring(spans[i].getEnd() - spans[0].getStart(), spans[i + 1].getStart() - spans[0].getStart());
 				}
 			} else {
-//				System.out.println(i + "  length " + spans[i].getLength() + " " + (Math.abs(spans[i].getStart() - spans[0].getStart())) + " " + (Math.abs(spans[i].getStart() - spans[0].getStart() - spans[i].getLength())));
-				seqArray[i] = seq.substring(Math.abs(spans[i].getStart() - spans[0].getStart()), Math.abs(spans[i].getStart() - spans[0].getStart() - spans[i].getLength()));
+				seqArray[i] = seq.substring(Math.abs(spans[i].getStart() - spans[0].getStart()), Math.abs(spans[i].getEnd() - spans[0].getStart()));
 				if (i < spans.length - 1) {
-					intronArray[i] = seq.substring(Math.abs(spans[i].getEnd() - spans[0].getStart() - 1), Math.abs(spans[i + 1].getStart() - spans[0].getStart() + 1));
+					intronArray[i] = seq.substring(Math.abs(spans[i].getEnd() - spans[0].getStart()), Math.abs(spans[i + 1].getStart() - spans[0].getStart()));
 				}
 			}
 			i++;
-//		System.out.println(i);
 		}
 
 		i = 0;
 
-//		while (spans.length > i) {
-//			System.out.println(seqArray[i] + " length " + seqArray[i].length());
-//			if (i < spans.length - 1) {
-//				System.out.println(intronArray[i] + " length " + intronArray[i].length());
-//			}
-//			i++;
-//		}
 		return;
 	}
 
@@ -194,49 +178,33 @@ public class NeoSeqDemo extends Applet
 
 		annotations = new Vector<GlyphI>();
 
-		
-
-		// need a Sequence data model since using FastaSequenceParser,
-		// which parses from fasta file into a Sequence
-		if (false) {
-			seq = seqmodel.getResidues();
-			seqview.setSequence(seqmodel);
+		seq = getClipboard();
+		if (null == spans1) {
+			seqview.setResidues(seq);
+			seqview.setResidueFontColor(Color.YELLOW);
 		} else {
-			
-//			seq = fake_seq;
-			seq = getClipboard();
-//			System.out.println(spans1[0] + "" + seq);
-			if(null == spans1){
-				seqview.setResidues(seq);
-			}
-			else{
-				String[] seqArray = new String[spans1.length];
-		String[] intronArray = new String[spans1.length - 1];
+			String[] seqArray = new String[spans1.length];
+			String[] intronArray = new String[spans1.length - 1];
 			customFormatting(seqArray, intronArray, spans1, seq);
-//			seqview.setResidues(seq);
-			int count =0;
-			for(int j=0,k=0,l=0;j<(2*spans1.length)-1;j++){
-				if((j%2) == 0){
+			//Below is done because NeoSeq has first character as white space
+			seqview.setResidues("");
+			int count = 0;
+			for (int j = 0, k = 0, l = 0; j < (2 * spans1.length) - 1; j++) {
+				if ((j % 2) == 0) {
 					seqview.appendResidues(seqArray[k]);
-					seqview.addTextColorAnnotation(count, (count+seqArray[k].length())-1, Color.LIGHT_GRAY);
-//					System.out.println(seqArray[k]);
-//					System.out.println(seqview.toString().substring(count, (count+seqArray[k].length())-1));
-//					System.out.println("extron "+seqArray[k].length());
-					count += seqArray[k].length();k++;
-					
-				}
-				else{
+					seqview.addTextColorAnnotation(count, (count + seqArray[k].length()) - 1, Color.YELLOW);
+					count += seqArray[k].length();
+					k++;
+
+				} else {
 					seqview.appendResidues(intronArray[l]);
-					seqview.addTextColorAnnotation(count, (count+intronArray[l].length())-1, Color.DARK_GRAY);
-//					System.out.println(intronArray[l]);
-//					System.out.println(seqview.toString().substring(count, (count+intronArray[l].length())-1));
-//					System.out.println("intron" +intronArray[l].length());
-					count += intronArray[l].length();l++;
-					
+					seqview.addTextColorAnnotation(count, (count + intronArray[l].length()) - 1, Color.WHITE);
+					count += intronArray[l].length();
+					l++;
+
 				}
-				System.out.println("count"+count+ "j "+j+"k "+k+"l "+l+" spans length"+spans1.length+"extron length "+seqArray.length+"intron length "+intronArray.length);
 			}
-		}
+
 		}
 
 		seqview.setShow(NeoSeq.COMPLEMENT, showComp);
@@ -246,8 +214,8 @@ public class NeoSeqDemo extends Applet
 			Color.black,
 			Color.black,};
 		seqview.setStripeColors(okayColors);
-		seqview.setFont(new Font("Courier", Font.BOLD, 14));
-		seqview.setResidueFontColor(Color.LIGHT_GRAY);
+		seqview.setFont(new Font("Arial", Font.BOLD, 14));
+
 		seqview.setNumberFontColor(Color.black);
 		seqview.setSpacing(20);
 
@@ -266,7 +234,7 @@ public class NeoSeqDemo extends Applet
 		 *  (such as Swing), you should _not_ wrap them with a NeoPanel
 		 *  (since the NeoPanel is a heavyweight component).
 		 */
-		NeoPanel widg_pan = new NeoPanel();
+		
 		widg_pan.setLayout(new BorderLayout());
 		widg_pan.add("Center", seqview);
 
@@ -279,7 +247,9 @@ public class NeoSeqDemo extends Applet
 		//    Dimension prefsize = seqview.getPreferredSize(50, 15);
 		//    seqview.resize(prefsize.width, prefsize.height);
 		//    mapframe.pack();  pack and menus don't mix well on some platforms...
-		mapframe.setSize(pixel_width, pixel_height);
+		Dimension prefsize = seqview.getPreferredSize(50, 15);
+		mapframe.setMinimumSize(prefsize);
+//		mapframe.setSize(pixel_width+230, pixel_height);
 		Dimension screen_size = Toolkit.getDefaultToolkit().getScreenSize();
 		mapframe.setLocation((screen_size.width - pixel_width) / 2,
 				(screen_size.height - pixel_height) / 2);
@@ -367,12 +337,11 @@ public class NeoSeqDemo extends Applet
 			try {
 				FileWriter fw = new FileWriter(fileName);
 				String r = seqview.getResidues();
-				System.out.println(seqview.getResidueColor());
 				fw.write(">" + fileName);
 				fw.write('\n');
 				int i;
-				for (i = 0; i < r.length() - 60; i += 60) {
-					fw.write(r, i, 60);
+				for (i = 0; i < r.length() - 50; i += 50) {
+					fw.write(r, i, 50);
 					fw.write('\n');
 				}
 				if (i < r.length()) {
@@ -398,6 +367,14 @@ public class NeoSeqDemo extends Applet
 			new MenuShortcut(KeyEvent.VK_A));
 	MenuItem exitMenuItem = new MenuItem("eXit",
 			new MenuShortcut(KeyEvent.VK_X));
+	Menu showMenu = new Menu("Show");
+	CheckboxMenuItem compCBMenuItem = new CheckboxMenuItem("Reverse Complement");
+	CheckboxMenuItem transOneCBMenuItem = new CheckboxMenuItem(" +1 Translation");
+	CheckboxMenuItem transTwoCBMenuItem = new CheckboxMenuItem(" +2 Translation");
+	CheckboxMenuItem transThreeCBMenuItem = new CheckboxMenuItem(" +3 Translation");
+	CheckboxMenuItem transNegOneCBMenuItem = new CheckboxMenuItem(" -1 Translation");
+	CheckboxMenuItem transNegTwoCBMenuItem = new CheckboxMenuItem(" -2 Translation");
+	CheckboxMenuItem transNegThreeCBMenuItem = new CheckboxMenuItem(" -3 Translation");
 
 	public void setupMenus(Frame dock) {
 
@@ -413,6 +390,21 @@ public class NeoSeqDemo extends Applet
 		fileMenu.add(exitMenuItem);
 		saveAsMenuItem.addActionListener(this);
 		exitMenuItem.addActionListener(this);
+		showMenu.add(compCBMenuItem);
+		showMenu.add(transOneCBMenuItem);
+		showMenu.add(transTwoCBMenuItem);
+		showMenu.add(transThreeCBMenuItem);
+		showMenu.add(transNegOneCBMenuItem);
+		showMenu.add(transNegTwoCBMenuItem);
+		showMenu.add(transNegThreeCBMenuItem);
+
+		compCBMenuItem.addItemListener(this);
+		transOneCBMenuItem.addItemListener(this);
+		transTwoCBMenuItem.addItemListener(this);
+		transThreeCBMenuItem.addItemListener(this);
+		transNegOneCBMenuItem.addItemListener(this);
+		transNegTwoCBMenuItem.addItemListener(this);
+		transNegThreeCBMenuItem.addItemListener(this);
 
 		// add the menus to the menubar
 		MenuBar bar = dock.getMenuBar();
@@ -423,10 +415,8 @@ public class NeoSeqDemo extends Applet
 
 		bar.add(fileMenu);
 		bar.add(editMenu);
+		bar.add(showMenu);
 	}
-
-
-
 
 	/* EVENT HANDLING */
 	/** ActionListener Implementation */
@@ -436,16 +426,16 @@ public class NeoSeqDemo extends Applet
 		if (theItem == copyMenuItem) {
 			String selectedSeq = seqview.getSelectedResidues();
 			if (selectedSeq != null) {
-					Clipboard clipboard = this.getToolkit().getSystemClipboard();
-					StringBuffer hackbuf = new StringBuffer(selectedSeq);
-					String hackstr = new String(hackbuf);
-					StringSelection data = new StringSelection(hackstr);
-					clipboard.setContents(data, null);
-				} else {
-					ErrorHandler.errorPanel("Missing Sequence Residues",
-							"Don't have all the needed residues, can't copy to clipboard.\n"
-							+ "Please load sequence residues for this region.");
-				}
+				Clipboard clipboard = this.getToolkit().getSystemClipboard();
+				StringBuffer hackbuf = new StringBuffer(selectedSeq);
+				String hackstr = new String(hackbuf);
+				StringSelection data = new StringSelection(hackstr);
+				clipboard.setContents(data, null);
+			} else {
+				ErrorHandler.errorPanel("Missing Sequence Residues",
+						"Don't have all the needed residues, can't copy to clipboard.\n"
+						+ "Please load sequence residues for this region.");
+			}
 
 		} else if (theItem == saveAsMenuItem) {
 			exportSequenceFasta();
@@ -456,107 +446,53 @@ public class NeoSeqDemo extends Applet
 
 	}
 
-//	private void setMenuItemState(Menu theMenu, CheckboxMenuItem theItem) {
-//		for (int i = theMenu.getItemCount() - 1; 0 <= i; i--) {
-//			MenuItem item = theMenu.getItem(i);
-//			if (item instanceof CheckboxMenuItem) {
-//				((CheckboxMenuItem) item).setState(item == theItem);
-//			}
-//		}
-//	}
-//
-//	/** ItemListener Implementation */
-//	public void itemStateChanged(ItemEvent e) {
-//		Object theItem = e.getSource();
-//
-//		if (theItem == oneLetterCBMenuItem) {
-//			seqview.setTranslationStyle(NeoSeq.ONE_LETTER_CODE);
-//			setMenuItemState(transFormatMenu, (CheckboxMenuItem) theItem);
-//			seqview.updateWidget();
-//		} else if (theItem == threeLetterCBMenuItem) {
-//			seqview.setTranslationStyle(NeoSeq.THREE_LETTER_CODE);
-//			setMenuItemState(transFormatMenu, (CheckboxMenuItem) theItem);
-//			seqview.updateWidget();
-//		} else if (theItem == compCBMenuItem) {
-//			CheckboxMenuItem mi = (CheckboxMenuItem) theItem;
-//			boolean showComp = mi.getState();
-//			seqview.setShow(NeoSeq.COMPLEMENT, showComp);
-//			seqview.updateWidget();
-//		} else if (theItem == transOneCBMenuItem) {
-//			CheckboxMenuItem mi = (CheckboxMenuItem) theItem;
-//			seqview.setShow(NeoSeq.FRAME_ONE, mi.getState());
-//			seqview.updateWidget();
-//		} else if (theItem == transTwoCBMenuItem) {
-//			CheckboxMenuItem mi = (CheckboxMenuItem) theItem;
-//			seqview.setShow(NeoSeq.FRAME_TWO, mi.getState());
-//			seqview.updateWidget();
-//		} else if (theItem == transThreeCBMenuItem) {
-//			CheckboxMenuItem mi = (CheckboxMenuItem) theItem;
-//			seqview.setShow(NeoSeq.FRAME_THREE, mi.getState());
-//			seqview.updateWidget();
-//		} else if (theItem == transNegOneCBMenuItem) {
-//			CheckboxMenuItem mi = (CheckboxMenuItem) theItem;
-//			seqview.setShow(NeoSeq.FRAME_NEG_ONE, mi.getState());
-//			seqview.updateWidget();
-//		} else if (theItem == transNegTwoCBMenuItem) {
-//			CheckboxMenuItem mi = (CheckboxMenuItem) theItem;
-//			seqview.setShow(NeoSeq.FRAME_NEG_TWO, mi.getState());
-//			seqview.updateWidget();
-//		} else if (theItem == transNegThreeCBMenuItem) {
-//			CheckboxMenuItem mi = (CheckboxMenuItem) theItem;
-//			seqview.setShow(NeoSeq.FRAME_NEG_THREE, mi.getState());
-//			seqview.updateWidget();
-//		} else if (theItem == dialogFontCBMenuItem
-//				|| theItem == dialogInputFontCBMenuItem
-//				|| theItem == monospacedFontCBMenuItem
-//				|| theItem == serifFontCBMenuItem
-//				|| theItem == sansSerifFontCBMenuItem) {
-//			seqview.setFontName(((MenuItem) theItem).getLabel());
-//			setMenuItemState(fontMenu, (CheckboxMenuItem) theItem);
-//		} else if (theItem == defaultFontCBMenuItem) {
-//			seqview.setFontName(""); // NeoSeq will use the Java default font.
-//			seqview.setFontName("Courier"); // This is the NeoSeq default font.
-//			setMenuItemState(fontMenu, (CheckboxMenuItem) theItem);
-//		} else if (theItem == smallFontCBMenuItem) {
-//			seqview.setFontSize(12);
-//			setMenuItemState(fontSizesMenu, (CheckboxMenuItem) theItem);
-//		} else if (theItem == mediumFontCBMenuItem) {
-//			seqview.setFontSize(14);
-//			setMenuItemState(fontSizesMenu, (CheckboxMenuItem) theItem);
-//		} else if (theItem == largeFontCBMenuItem) {
-//			seqview.setFontSize(16);
-//			setMenuItemState(fontSizesMenu, (CheckboxMenuItem) theItem);
-//		} else if (theItem == vertStripesCBMenuItem) {
-//			seqview.setStripeOrientation(NeoSeq.VERTICAL_STRIPES);
-//			setMenuItemState(stripesMenu, (CheckboxMenuItem) theItem);
-//		} else if (theItem == horizStripesCBMenuItem) {
-//			seqview.setStripeOrientation(NeoSeq.HORIZONTAL_STRIPES);
-//			setMenuItemState(stripesMenu, (CheckboxMenuItem) theItem);
-//		} else if (theItem == noStripesCBMenuItem) {
-//			seqview.setStripeOrientation(NeoSeq.NO_STRIPES);
-//			setMenuItemState(stripesMenu, (CheckboxMenuItem) theItem);
-//		} else if (theItem == smallStripesCBMenuItem) {
-//			seqview.setStripeWidth(5);
-//			setMenuItemState(stripeSizesMenu, (CheckboxMenuItem) theItem);
-//		} else if (theItem == mediumStripesCBMenuItem) {
-//			seqview.setStripeWidth(10);
-//			setMenuItemState(stripeSizesMenu, (CheckboxMenuItem) theItem);
-//		} else if (theItem == largeStripesCBMenuItem) {
-//			seqview.setStripeWidth(20);
-//			setMenuItemState(stripeSizesMenu, (CheckboxMenuItem) theItem);
-//		} else if (theItem == smallLineSpacingCBMenuItem) {
-//			seqview.setSpacing(0);
-//			setMenuItemState(lineSpacingMenu, (CheckboxMenuItem) theItem);
-//		} else if (theItem == mediumLineSpacingCBMenuItem) {
-//			seqview.setSpacing(10);
-//			setMenuItemState(lineSpacingMenu, (CheckboxMenuItem) theItem);
-//		} else if (theItem == largeLineSpacingCBMenuItem) {
-//			seqview.setSpacing(20);
-//			setMenuItemState(lineSpacingMenu, (CheckboxMenuItem) theItem);
-//		}
-//
-//	}
-//
+	private void setMenuItemState(Menu theMenu, CheckboxMenuItem theItem) {
+		for (int i = theMenu.getItemCount() - 1; 0 <= i; i--) {
+			MenuItem item = theMenu.getItem(i);
+			if (item instanceof CheckboxMenuItem) {
+				((CheckboxMenuItem) item).setState(item == theItem);
+			}
+		}
+	}
+
+	/** ItemListener Implementation */
+	public void itemStateChanged(ItemEvent e) {
+		Object theItem = e.getSource();
+
+		if (theItem == compCBMenuItem) {
+			CheckboxMenuItem mi = (CheckboxMenuItem) theItem;
+			boolean showRevComp = mi.getState();
+			seqview.setRevShow(NeoSeq.COMPLEMENT, showRevComp);
+			seqview.setRevShow(NeoSeq.NUCLEOTIDES, !showRevComp);
+			seqview.updateWidget();
+		} else if (theItem == transOneCBMenuItem) {
+			CheckboxMenuItem mi = (CheckboxMenuItem) theItem;
+			seqview.setShow(NeoSeq.FRAME_ONE, mi.getState());
+			seqview.updateWidget();
+		} else if (theItem == transTwoCBMenuItem) {
+			CheckboxMenuItem mi = (CheckboxMenuItem) theItem;
+			seqview.setShow(NeoSeq.FRAME_TWO, mi.getState());
+			seqview.updateWidget();
+		} else if (theItem == transThreeCBMenuItem) {
+			CheckboxMenuItem mi = (CheckboxMenuItem) theItem;
+			seqview.setShow(NeoSeq.FRAME_THREE, mi.getState());
+			seqview.updateWidget();
+		} else if (theItem == transNegOneCBMenuItem) {
+			CheckboxMenuItem mi = (CheckboxMenuItem) theItem;
+			seqview.setShow(NeoSeq.FRAME_NEG_ONE, mi.getState());
+			seqview.updateWidget();
+		} else if (theItem == transNegTwoCBMenuItem) {
+			CheckboxMenuItem mi = (CheckboxMenuItem) theItem;
+			seqview.setShow(NeoSeq.FRAME_NEG_TWO, mi.getState());
+			seqview.updateWidget();
+		} else if (theItem == transNegThreeCBMenuItem) {
+			CheckboxMenuItem mi = (CheckboxMenuItem) theItem;
+			seqview.setShow(NeoSeq.FRAME_NEG_THREE, mi.getState());
+			seqview.updateWidget();
+		} 
+
+	}
+
 	/** WindowListener Implementation */
 	public void windowActivated(WindowEvent e) {
 	}
@@ -623,7 +559,5 @@ public class NeoSeqDemo extends Applet
 		me.start(spans);
 	}
 
-	public void itemStateChanged(ItemEvent e) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+
 }
