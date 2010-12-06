b3db724ef4b5d8b3a7801a79ea1468fd40ebb2b9
paetow <paetow@a82c77b2-4dfe-7ae9-12ac-60ed84af8d34>
2010-12-06 15:52:48 +0000

    CommandLine läuft nun

--- genoviz/igb/src/com/affymetrix/igb/util/CommandLineWindow.java
+++ genoviz/igb/src/com/affymetrix/igb/util/CommandLineWindow.java
@@ -15,6 +15,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.Vector;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -31,16 +32,20 @@ public class CommandLineWindow extends JFrame implements KeyListener{
 	private InputStream stderr;
 
 	private JTextArea output_area;
+	private JScrollPane output_scroll;
 	private JTextField input_field;
+
+	private int lineCnt = 1;
 
 	public CommandLineWindow() {
 		this.setTitle("Commandline test");
 		this.setLayout(new BorderLayout());
 		output_area = new JTextArea(50, 80);
 		output_area.setEditable(false);
+		output_scroll = new JScrollPane(output_area);
 		input_field = new JTextField(80);
 		input_field.addKeyListener(this);
-		this.add(output_area, BorderLayout.CENTER);
+		this.add(output_scroll, BorderLayout.CENTER);
 		this.add(input_field, BorderLayout.SOUTH);
 		this.init();
 
@@ -60,42 +65,41 @@ public class CommandLineWindow extends JFrame implements KeyListener{
 	}
 
 	private void sendStringToConsole(String s){
-		try{
-			String cmd = s+"\n";
-			stdin.write(s.getBytes());
-			stdin.flush();
-		}catch (Exception e){
-			System.out.println("ex write stdin");
+		if(s.length() > 0){
+			String[] commands;
+			int cmdCnt = 0;
+			String os = (String) System.getProperties().get("os.name");
+			if(os.contains("win")){
+				commands = new String[2];
+				commands[cmdCnt++] = "cmd.exe";
+			}else if(os.contains("nix")|| os.contains("nux")){
+				commands = new String[3];
+				commands[cmdCnt++] = "/bin/bash";
+				commands[cmdCnt++] = "-c";
+			}else if(os.contains("mac")){
+				commands = new String[2];
+				commands[cmdCnt++] = ""; //TODO welche shell bei MacOS ??
+			}else{
+				commands = new String[1];
+			}
+			commands[cmdCnt++] = s+"\n";
+			try{
+				process = Runtime.getRuntime().exec(commands);
+				stdin = process.getOutputStream();
+				stdout = process.getInputStream();
+				stderr = process.getErrorStream();
+				String actLine = "";
+				BufferedReader brCleanUp =
+					new BufferedReader (new InputStreamReader(stdout));
+				while((actLine = brCleanUp.readLine())!= null){
+					output_area.append(actLine + "\n");
+				}
+			}catch(Exception ioe){
+				System.out.println(ioe);
+			}
 		}
 	}
-	private void readConsoleOutput(){
-		try{
-			String[] lines = output_area.getText().split("\n");
-			Vector<String> newLines = new Vector<String>();
-			for (int i = 0; i < lines.length; i++) {
-				newLines.add(lines[i]);
-			}
-			String actLine = "";
-			BufferedReader brCleanUp = 
-				new BufferedReader (new InputStreamReader(stdout));
-			System.out.println(brCleanUp.ready());
-			if(brCleanUp.ready()){
-				do{
-						actLine = brCleanUp.readLine();
-						newLines.add(actLine+ "\n");
-				}while(actLine != null);
-			}
-			int numLines = newLines.size();
-			if(numLines > 50) numLines = 50;
-			for (int i = 0; i < numLines; i++) {
-				actLine += newLines.get(newLines.size()-i-1);
-			}
-			brCleanUp.close();
-		}catch (IOException e){
-			System.out.println("ex read stdout");
-			e.printStackTrace();
-		}
-	}
+
 
 	public void keyTyped(KeyEvent ke) {
 	}
@@ -105,7 +109,6 @@ public class CommandLineWindow extends JFrame implements KeyListener{
 		if(ke.getKeyChar() == KeyEvent.VK_ENTER){
 			this.sendStringToConsole(input_field.getText());
 			input_field.setText("");
-			this.readConsoleOutput();
 		}
 	}
 
