67d4c70efa6549019998ff5ccd0e2629fc5a1025
wienhoef <wienhoef@a82c77b2-4dfe-7ae9-12ac-60ed84af8d34>
2010-12-11 11:37:09 +0000

    [merging genoviz commit 7152]
    original comment:
    removed ensembl view instantiation from externalviewer until ensembl url problems are resolve

--- genoviz/igb/src/com/affymetrix/igb/view/external/ExternalViewer.java
+++ genoviz/igb/src/com/affymetrix/igb/view/external/ExternalViewer.java
@@ -13,22 +13,18 @@ import javax.swing.JComponent;
  *
  * @author Ido M. Tamir
  */
-public class ExternalViewer extends JComponent implements ItemListener {
+public class ExternalViewer extends JComponent {
 
-	private static final String[] names = {UCSCView.viewName, EnsemblView.viewName};
+	private static final String[] names = {UCSCView.viewName};
 	final JComboBox ucscBox;
-	final JComboBox ensemblBox;
 
 	public ExternalViewer() {
 		this.setLayout(new CardLayout());
 		ucscBox = createBox();
-		ensemblBox = createBox();
 
 		final UCSCView ucsc = new UCSCView(ucscBox);
-		final EnsemblView ensembl = new EnsemblView(ensemblBox);
 
 		add(ucsc, ucsc.getViewName());
-		add(ensembl, ensembl.getViewName());
 	}
 
 	private JComboBox createBox() {
@@ -36,21 +32,8 @@ public class ExternalViewer extends JComponent implements ItemListener {
 		box.setPrototypeDisplayValue("ENSEMBL");
 		box.setMaximumSize(box.getPreferredSize());
 		box.setEditable(false);
-		box.addItemListener(this);
 		return box;
 	}
 
 	
-	public void itemStateChanged(ItemEvent e) {
-		if (e.getID() == ItemEvent.ITEM_STATE_CHANGED) {
-			CardLayout cl = (CardLayout) (getLayout());
-			if (e.getSource() == ucscBox) {
-				ensemblBox.setSelectedItem(EnsemblView.viewName);
-			}
-			if (e.getSource() == ensemblBox) {
-				ucscBox.setSelectedItem(UCSCView.viewName);
-			}
-			cl.show(ExternalViewer.this, (String) e.getItem());
-		}
-	}
 }
